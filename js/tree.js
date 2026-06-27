5e003a7e3611b90a6484cf6ab54914a3bd04d51b
chakalis49-coder <sounds.virtual.mix49@gmail.com>
2026-06-27 18:07:28 +0200

    mise à jour site agence

--- js/tree.js
+++ js/tree.js
@@ -1,5 +1,5 @@
 const width = 850;
-const height = 700; // ✅ un peu plus grand pour éviter coupure
+const height = 1100; // ✅ un peu plus grand pour éviter coupure
 const offsetY = -20;
 
 // ✅ SVG RESPONSIVE (FIX PRINCIPAL)
@@ -14,24 +14,28 @@ const svg = d3.select("#tree")
 // ------------------
 
 const nodes = [
-    { id: "profil-fondateur1", name: "Fondateur Pépito", x: 283, y: 80 + offsetY, role: "founder", img: "img/fondateur_1.jpeg" },
-    { id: "profil-fondateur2", name: "Fondateur Vikédium", x: 567, y: 80 + offsetY, role: "founder", img: "img/fondateur_2.jpeg" },
+    { id: "profil-fondateur1", name: "Fondateur Pépito", x: 283, y: 80 + offsetY, role: "founder", img: "img/pepito_blason.jpeg" },
+    { id: "profil-fondateur2", name: "Fondateur Vikédium", x: 567, y: 80 + offsetY, role: "founder", img: "img/vikidium_blason.jpeg" },
 
-    { id: "profil-directrice1", name: "Directrice Papillons", x: 283, y: 220 + offsetY, role: "director", img: "img/directrice_1.jpeg" },
-    { id: "profil-directrice2", name: "Directrice Pockettelande", x: 567, y: 220 + offsetY, role: "director", img: "img/directrice_2.jpeg" },
+    { id: "profil-directrice1", name: "Directrice Papillons", x: 150, y: 220 + offsetY, role: "director", img: "img/papillion_blason.jpeg" },
+    { id: "profil-directrice2", name: "Directrice Pockettelande", x: 700, y: 220 + offsetY, role: "director", img: "img/pocastich_blason.jpeg" },
+		
+    { id: "profil-coordinateur", name: "Coordinateur Chakalis49", x: 425, y: 360 + offsetY, role: "coordinateur", img: "img/coordinateur.png" },
 
-    { id: "profil-manager1", name: "Manageur Nexuzio", x: 150, y: 380 + offsetY, role: "manager", img: "img/manager_1.jpeg" },
-    { id: "profil-manager2", name: "Manageuse Licorne", x: 425, y: 380 + offsetY, role: "manager", img: "img/manager_2.jpeg" },
-    { id: "profil-manager3", name: "Manageur Nounours", x: 700, y: 380 + offsetY, role: "manager", img: "img/manager_3.jpeg" },
+    { id: "profil-manager1", name: "Manageuse Nadia", x: 150, y: 520 + offsetY, role: "manager", img: "img/nadia_blason.jpeg" },
+    { id: "profil-manager2", name: "Manageuse Licorne", x: 425, y: 520 + offsetY, role: "manager", img: "img/licorne_blason.jpeg" },
+    { id: "profil-manager3", name: "Manageur Jerem", x: 700, y: 520 + offsetY, role: "manager", img: "img/jerem_blason.jpeg" },
 
-    { id: "profil-agent1", name: "Agent Artémis", x: 150, y: 500 + offsetY, img: "img/agent_1.jpeg" },
-    { id: "profil-agent2", name: "Agent Chelsea", x: 150, y: 620 + offsetY, img: "img/agent_2.jpeg" },
+    { id: "profil-agent_tony", name: "Agent Tony", x: 150, y: 640 + offsetY, img: "img/tony_blason.jpeg" },
 
-    { id: "profil-agent3", name: "Agent Jerem", x: 425, y: 500 + offsetY, img: "img/agent_3.jpeg" },
-    { id: "profil-agent4", name: "Agente Manon", x: 425, y: 620 + offsetY, img: "img/agent_4.jpeg" },
-
-    { id: "profil-agent5", name: "Agente Dania", x: 700, y: 500 + offsetY, img: "img/agent_5.jpeg" },
-    { id: "profil-agent6", name: "Agent Chantal", x: 700, y: 620 + offsetY, img: "img/agent_6.jpeg" }
+    { id: "profil-agent_manon", name: "Agente Manon", x: 425, y: 640 + offsetY, img: "img/manon_blason.jpeg" },
+		{ id: "profil-agent_chantal", name: "Agent Chantal", x: 425, y: 780 + offsetY, img: "img/chantal_blason.jpeg" },
+		{ id: "profil-agent_emi", name: "Agente Émi", x: 425, y: 920 + offsetY, img: "img/emi_blason.jpeg" },
+		{ id: "profil-agent_chelsea", name: "Agent Chelsea", x: 425, y: 1060 + offsetY, img: "img/chelsea_blason.jpeg" },
+		
+    { id: "profil-agent_artemis", name: "Agent Artémis", x: 700, y: 640 + offsetY, img: "img/agent_artemis.jpeg" },
+		{ id: "profil-agent_loup_blanc", name: "Agent Loup Blanc", x: 700, y: 780 + offsetY, img: "img/loup_blanc_blason.jpeg" },
+    { id: "profil-agent_loulou", name: "Agent Loulou", x: 700, y: 920 + offsetY, img: "img/agent_loulou.jpeg" }
 ];
 
 // ------------------
@@ -47,19 +51,23 @@ const links = [
     ["profil-directrice1", "profil-manager1"],
     ["profil-directrice1", "profil-manager2"],
     ["profil-directrice1", "profil-manager3"],
+    ["profil-directrice1", "profil-coordinateur"],
 
     ["profil-directrice2", "profil-manager1"],
     ["profil-directrice2", "profil-manager2"],
     ["profil-directrice2", "profil-manager3"],
+    ["profil-directrice2", "profil-coordinateur"],
 
-    ["profil-manager1", "profil-agent1"],
-    ["profil-manager1", "profil-agent2"],
+		["profil-manager1", "profil-agent_tony"],
 
-    ["profil-manager2", "profil-agent3"],
-    ["profil-manager2", "profil-agent4"],
+    ["profil-manager2", "profil-agent_manon"],
+    ["profil-manager2", "profil-agent_chantal"],
+    ["profil-manager2", "profil-agent_emi"],
+    ["profil-manager2", "profil-agent_chelsea"],
 
-    ["profil-manager3", "profil-agent5"],
-    ["profil-manager3", "profil-agent6"]
+    ["profil-manager3", "profil-agent_artemis"],
+    ["profil-manager3", "profil-agent_loup_blanc"],
+    ["profil-manager3", "profil-agent_loulou"]
 ];
 
 // ------------------
@@ -148,6 +156,7 @@ node.append("rect")
     .attr("stroke", d => {
         if (d.role === "founder") return "#00cfff";
         if (d.role === "director") return "#00ffcc";
+        if (d.role === "coordinateur") return "#ffffff";
         if (d.role === "manager") return "#ff00cc";
         return "#ffffff33";
     })
